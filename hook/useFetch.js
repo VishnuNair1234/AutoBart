1e1c6034cc4772af6154a3605cb4995146f1d096
Vishnu Narayan Nair <vishnunarayans@hotmail.com>
2023-09-24 21:29:43 -0400

    named the app and added api hook file

---
+++ hook/useFetch.js
@@ -0,0 +1,13 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+const useFetch = () => {
+    const [data, setData] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+
+    const options = {
+        
+    }
+
+}
